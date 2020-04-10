d42a3671f4eba73b1d7311d9e0527afba55bf104
EriRan <erik.rantanen@hotmail.fi>
2020-04-10 17:53:23 +0300

    Add missing parts from actions and reducers. Data not being fetched due to there being not Access-Control-Allow-Origin in the header

--- src/reducers/publicTransportDataReducer.js
+++ src/reducers/publicTransportDataReducer.js
@@ -1,12 +1,17 @@
-import {
-  GET_DATA
-} from "../actions/types";
+import { GET_DATA } from "../actions/types";
 
 export default (state = {}, action) => {
   switch (action.type) {
     case GET_DATA:
-      print(action.payload);
-      return {...state, [action.payload.id]: action.payload};
+      console.log("At getDataReducer");
+      return [
+        ...state,
+        {
+          stops: action.payload.pysakit,
+          roads: action.payload.tiet,
+          busLines: action.payload.linjastot,
+        },
+      ];
     default:
       return state;
   }
