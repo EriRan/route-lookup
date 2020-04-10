561e56b3ed6f35655ae993b2f415b7d188d56e59
ornom0n <ornom0n@hotmail.com>
2020-04-10 17:53:23 +0300

    Add missing parts from actions and reducers. Data not being fetched due to there being not Access-Control-Allow-Origin in the header

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,11 @@
-import routeDataApi from "../apis/transport-data"
+import transportDataApi from "../apis/solidabisTransportData"
 import {
   GET_DATA
 } from "./types";
 
 export const getPublicTransportData = () => async dispatch => {
-  const response = await routeDataApi.get();
+  console.log("Getting public transport data");
+  const response = await transportDataApi.get();
   //Validate response contains: 1. Stops 2. Connections between stops 3. Public transport routes
   //If no response, use local backup
   dispatch({ type: GET_DATA, payload: response.data });
--- src/components/map/MapView.js
+++ src/components/map/MapView.js
@@ -4,17 +4,28 @@ import { getPublicTransportData } from "../../actions";
 
 class MapView extends React.Component {
   componentDidMount() {
+    console.log("Component mounting");
     this.props.getPublicTransportData();
   }
 
   render() {
-    return <div>MapView</div>;
+    console.log("Rendering");
+    return (
+      <div>
+        <p>Stops: {this.props.stops.size}</p>
+        <p>Roads: {this.props.roads.size}</p>
+        <p>Bus lines : {this.props.busLines.size}</p>
+      </div>
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
+  console.log("Mapping state");
   return {
-    publicTransportData: state.publicTransportData,
+    stops: state.stops != null ? state.stops : [],
+    roads: state.roads != null ? state.roads : [],
+    busLines: state.busLines != null ? state.busLines : [],
   };
 };
 
