42df0b36ace98a3ef857e2c6529c8e00e226f333
ornom0n <ornom0n@hotmail.com>
2020-04-10 19:14:01 +0300

    Clean code a bit

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,5 @@ import {
 export const getPublicTransportData = () => async dispatch => {
   //Validate response contains: 1. Stops 2. Connections between stops 3. Public transport routes
   //If no response, use local backup
-  let mapper = new TransportDataMapper();
-
-  dispatch({ type: GET_DATA, payload: mapper.map(transportData)});
+  dispatch({ type: GET_DATA, payload: new TransportDataMapper().map(transportData)});
 };
