aeb2fc56f6938c174f674fb0a686e4926c32f08d
EriRan <erik.rantanen@hotmail.fi>
2020-04-15 19:27:10 +0300

    Change bus stop location provider algorithm
    Bus stop provider algorithm now provides a prettier result, but there
    are still many overlaps.

--- src/components/map/markers/stop/BusStopLocationProvider.js
+++ src/components/map/markers/stop/BusStopLocationProvider.js
@@ -1,60 +1,101 @@
-import _ from "lodash";
-
 import { FIRST_LOCATION } from "./BusStopConstant";
 import { provideNextLocation } from "./NextLocationProvider";
 import { RIGHT } from "./NextBusStopDirection";
+import { isUndefinedOrNull } from "../../../../util/Utilities";
 
 class BusStopLocationProvider {
   provide(firstStop) {
     const alreadyDeducedStops = new Map();
-    this.addOneLocation(alreadyDeducedStops, firstStop, FIRST_LOCATION);
+    const alreadyIncludedRoads = [];
+    alreadyDeducedStops.set(firstStop.name, {
+      x: FIRST_LOCATION.x,
+      y: FIRST_LOCATION.y,
+    });
+    this.addNeighbours(
+      alreadyDeducedStops,
+      alreadyIncludedRoads,
+      firstStop,
+      FIRST_LOCATION
+    );
     return alreadyDeducedStops;
   }
 
-  addOneLocation(alreadyDeducedStops, stopData, location) {
-    const alreadyDeducedStop = alreadyDeducedStops.get(stopData.name);
-    if (_.isUndefined(alreadyDeducedStop) || _.isNull(alreadyDeducedStop)) {
-      alreadyDeducedStops.set(stopData.name, {
-        x: location.x,
-        y: location.y,
-      });
-      const nextDirectionIndexContainer = { next: RIGHT };
-      stopData.roads
-        .filter((road) => {
-          return road.isReverse === false
-        })
-        .forEach((road) => {
-          this.addOneLocation(
-            alreadyDeducedStops,
-            road.to,
-            this.deduceNextLocation(
-              road.to,
-              nextDirectionIndexContainer,
-              alreadyDeducedStops,
-              location
-            )
-          );
-        });
-    }
-  }
-
-  deduceNextLocation(
-    nextStop,
-    nextDirectionIndexContainer,
-    renderedStops,
+  addNeighbours(
+    alreadyDeducedStops,
+    alreadyIncludedRoads,
+    stopData,
     currentLocation
   ) {
-    const alreadyDeducedStop = renderedStops.get(nextStop.name);
-    if (_.isUndefined(alreadyDeducedStop) || _.isNull(alreadyDeducedStop)) {
-      const nextLocation = provideNextLocation(
-        currentLocation,
-        nextDirectionIndexContainer.next
+    const neighbourStops = [];
+    const nextDirectionIndexContainer = { next: RIGHT };
+    this.deduceNeighbours(
+      stopData,
+      alreadyIncludedRoads,
+      alreadyDeducedStops,
+      nextDirectionIndexContainer,
+      currentLocation,
+      neighbourStops
+    );
+    neighbourStops.forEach((neighbourStop) => {
+      this.addNeighbours(
+        alreadyDeducedStops,
+        alreadyIncludedRoads,
+        neighbourStop.stopData,
+        neighbourStop.location
       );
-      nextDirectionIndexContainer.next++;
-      return nextLocation;
-    } else {
-      return null;
-    }
+    });
+  }
+
+  deduceNeighbours(
+    stopData,
+    alreadyIncludedRoads,
+    alreadyDeducedStops,
+    nextDirectionIndexContainer,
+    currentLocation,
+    neighbourStops
+  ) {
+    stopData.roads.forEach((road) => {
+      if (this.isRoadAlreadyIncluded(road, alreadyIncludedRoads)) {
+        return;
+      }
+      alreadyIncludedRoads.push({
+        fromName: road.from.name,
+        toName: road.to.name,
+      });
+      const potentialNeighbour = alreadyDeducedStops.get(road.to.name);
+      if (!isUndefinedOrNull(potentialNeighbour)) {
+        return;
+      }
+      const nextLocation = this.deduceNextLocation(
+        nextDirectionIndexContainer,
+        currentLocation
+      );
+      alreadyDeducedStops.set(road.to.name, nextLocation);
+      neighbourStops.push({
+        stopData: road.to,
+        location: nextLocation,
+      });
+    });
+  }
+
+  deduceNextLocation(nextDirectionIndexContainer, currentLocation) {
+    const nextLocation = provideNextLocation(
+      currentLocation,
+      nextDirectionIndexContainer.next
+    );
+    nextDirectionIndexContainer.next++;
+    return nextLocation;
+  }
+
+  isRoadAlreadyIncluded(road, alreadyIncludedRoads) {
+    return alreadyIncludedRoads.some((alreadyIncludedRoad) => {
+      return (
+        (road.toName === alreadyIncludedRoad.toName ||
+          road.toName === alreadyIncludedRoad.fromName) &&
+        (road.fromName === alreadyIncludedRoad.fromName ||
+          road.fromName === alreadyIncludedRoad.toName)
+      );
+    });
   }
 }
 
