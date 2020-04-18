4a0dad7e362e8f4b0ed457a00602be4aedbbc999
EriRan <erik.rantanen@hotmail.fi>
2020-04-18 15:23:21 +0300

    Add a simple sidebar
    Goes over most of the content for now

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import MapView from "./map/MapView";
+import Sidebar from "./sidebar/Sidebar";
 
 const App = () => {
   return (
     <div className="ui container">
+      <Sidebar />
       <MapView />
     </div>
   );
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,27 @@
 import React from "react";
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import "./Sidebar.css";
 
 class Sidebar extends React.Component {
   render() {
-    return <div>Sidebar</div>;
+    return (
+      <div className="sidebar">
+      <List disablePadding dense>
+        <ListItem button>
+          <ListItemText>Home</ListItemText>
+        </ListItem>
+        <ListItem button>
+          <ListItemText>Billing</ListItemText>
+        </ListItem>
+        <ListItem button>
+          <ListItemText>Settings</ListItemText>
+        </ListItem>
+      </List>
+      </div>
+    )
   }
 }
+
+export default Sidebar;
