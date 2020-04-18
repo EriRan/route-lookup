4e7694861359e2ac7af8eefe5bc691360f656f42
EriRan <erik.rantanen@hotmail.fi>
2020-04-18 16:47:47 +0300

    Add required components for the sidebar
    Lot of styling is still required

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,25 +1,69 @@
 import React from "react";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import { Drawer } from "@material-ui/core";
+import {
+  Button,
+  Drawer,
+  Divider,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Typography
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import TextField from "@material-ui/core/TextField";
 import "./Sidebar.css";
 
 class Sidebar extends React.Component {
   render() {
     return (
       <Drawer anchor={"left"} open={true} variant="persistent">
-        <List disablePadding dense>
-          <ListItem button>
-            <ListItemText>Home</ListItemText>
-          </ListItem>
-          <ListItem button>
-            <ListItemText>Billing</ListItemText>
-          </ListItem>
-          <ListItem button>
-            <ListItemText>Settings</ListItemText>
-          </ListItem>
-        </List>
+        <p>Kuutiola reittiopas</p>
+        <Divider />
+        <form>
+          <TextField
+            id="outlined-basic"
+            label="Lähtöpaikka"
+            variant="outlined"
+          />
+
+          <TextField id="outlined-basic" label="Määränpää" variant="outlined" />
+        </form>
+        <Divider />
+        <p>Keltainen linja: A -> D</p>
+        <p>Punainen linja: D -> Q</p>
+        <p>Kesto: 12</p>
+        <Divider />
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Linjatiedot</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              <p>Keltainen <Button>Näytä</Button></p>
+              <p>Punainen <Button>Näytä</Button></p>
+              <p>Vihreä <Button>Näytä</Button></p>
+              <p>Sininen <Button>Näytä</Button></p>
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography>Tietoa sivusta</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              <p>Tehty Solidabiksen koodihaastetta varten</p>
+              <p>Koodaus ja ulkoasu: Erik Rantanen</p>
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
       </Drawer>
     );
   }
