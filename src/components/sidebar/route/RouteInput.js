767fb7c3398cc9bdeb364a38e13754efcde101b3
EriRan <erik.rantanen@hotmail.fi>
2020-04-19 14:59:53 +0300

    Add grid to center contents
    Looks pretty bad with the header though which isn't in the same center
    alignment

--- src/components/sidebar/route/RouteInput.js
+++ src/components/sidebar/route/RouteInput.js
@@ -1,16 +1,30 @@
 import React from "react";
 
-import { TextField, Typography } from "@material-ui/core";
+import { TextField, Typography, Grid } from "@material-ui/core";
 
 class RouteInput extends React.Component {
   render() {
     return (
       <form>
-        <Typography align="center">Mistä lähdet?</Typography>
-        <TextField id="outlined-basic" label="Lähtöpaikka" variant="outlined" />
-        
-        <Typography align="center">Minne haluat mennä?</Typography>
-        <TextField id="outlined-basic" label="Määränpää" variant="outlined" />
+        <Grid container alignItems="center" direction="column">
+          <Typography>Mistä lähdet?</Typography>
+          <TextField
+            id="outlined-basic"
+            className="center-input"
+            label="Lähtöpaikka"
+            variant="outlined"
+            margin="dense"
+          />
+
+          <Typography>Minne haluat mennä?</Typography>
+          <TextField
+            id="outlined-basic"
+            className="center-input"
+            label="Määränpää"
+            variant="outlined"
+            margin="dense"
+          />
+        </Grid>
       </form>
     );
   }
