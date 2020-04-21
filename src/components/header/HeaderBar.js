9dd540b6f5191c0c1eb790beb33c141ad9944cd4
EriRan <erik.rantanen@hotmail.fi>
2020-04-21 20:05:07 +0300

    Center title in header

--- src/components/header/HeaderBar.js
+++ src/components/header/HeaderBar.js
@@ -1,21 +1,24 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
 
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import SidebarOpenButton from "./SidebarOpenButton";
 
-class HeaderBar extends React.Component {
-  render() {
-    return (
-      <AppBar position="fixed" color="primary">
-        <Toolbar>
-          <SidebarOpenButton/>
-          <Typography variant="h5" align="right">
-            Kuutiola reittiopas
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    );
-  }
-}
+const styles = {
+  centeredTitle: {
+    margin: "0 auto",
+  },
+};
 
-export default HeaderBar;
+const HeaderBar = ({ classes }) => (
+  <AppBar position="fixed" color="primary">
+    <Toolbar>
+      <SidebarOpenButton />
+      <Typography variant="h5" align="right" className={classes.centeredTitle}>
+        Kuutiola reittiopas
+      </Typography>
+    </Toolbar>
+  </AppBar>
+);
+
+export default withStyles(styles)(HeaderBar);
