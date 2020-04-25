a8ea24fb9fdb04a49f2370865f27a8ef044f6ede
EriRan <erik.rantanen@hotmail.fi>
2020-04-25 13:14:04 +0300

    Add routeReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import sidebarReducer from "./sidebarReducer";
+import routeReducer from "./routeReducer";
 
 export default combineReducers({
   form: formReducer,
-  sidebar: sidebarReducer
+  sidebar: sidebarReducer,
+  route: routeReducer,
 });
