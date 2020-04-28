6a0cf4da52427994b86d3bde9e1b04e2f6b49a3b
ornom0n <ornom0n@hotmail.com>
2020-04-28 19:36:29 +0300

    Render all lines available for a road
    All lines are rendered in the color of the line in Finnish.
    When a road contains more than one lines, we render the next ones
    5 pixels away from the last.

--- src/components/route/map/markers/road/RoadLine.js
+++ src/components/route/map/markers/road/RoadLine.js
@@ -1,15 +1,16 @@
 import React from "react";
 import RoadLineDuration from "./RoadLineDuration";
 import RoadStyleDeducer from "./RoadStyleDeducer";
+import { LINE_GAP } from "./RoadConstant";
 
 class RoadLine extends React.Component {
   render() {
     return (
       <g className="road-line">
-        {this.renderLine(
+        {this.renderLines(
           this.props.startPointLocation,
           this.props.endPointLocation,
-          new RoadStyleDeducer().deduce(this.props.roadData.includesLines)
+          this.props.roadData
         )}
         <RoadLineDuration
           startPointLocation={this.props.startPointLocation}
@@ -20,17 +21,58 @@ class RoadLine extends React.Component {
     );
   }
 
-  renderLine(startPointLocation, endPoint, styleObject) {
+  renderLines(startPointLocation, endPoint, roadData) {
+    const styleObjects = new RoadStyleDeducer().deduce(
+      this.props.roadData.includesLines
+    );
+    const linesToRender = [];
+    for (let i = 0; i < styleObjects.length; i++) {
+      linesToRender.push(
+        this.renderOneLine(
+          startPointLocation,
+          endPoint,
+          roadData,
+          styleObjects[i],
+          i
+        )
+      );
+    }
+    return linesToRender;
+  }
+
+  renderOneLine(startPointLocation, endPoint, roadData, styleObject, index) {
+    if (this.isLineHorizontal(startPointLocation.x, endPoint.x)) {
+      return (
+        <line
+          key={`line-${roadData.from.name}-${roadData.to.name}-${styleObject.color}`}
+          x1={startPointLocation.x}
+          y1={startPointLocation.y + this.distanceFromOtherLine(index)}
+          x2={endPoint.x}
+          y2={endPoint.y + this.distanceFromOtherLine(index)}
+          stroke={styleObject.color}
+          opacity={styleObject.opacity}
+        />
+      );
+    }
     return (
       <line
-        x1={startPointLocation.x}
+        key={`line-${roadData.from.name}-${roadData.to.name}-${styleObject.color}`}
+        x1={startPointLocation.x + this.distanceFromOtherLine(index)}
         y1={startPointLocation.y}
-        x2={endPoint.x}
+        x2={endPoint.x + this.distanceFromOtherLine(index)}
         y2={endPoint.y}
         stroke={styleObject.color}
         opacity={styleObject.opacity}
       />
     );
+  }
+
+  distanceFromOtherLine(index) {
+    return index * LINE_GAP;
+  }
+
+  isLineHorizontal(xOne, xTwo) {
+    return xOne === xTwo;
   }
 }
 
