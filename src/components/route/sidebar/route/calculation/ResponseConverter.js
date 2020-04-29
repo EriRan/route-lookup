fb52a6a2b2cf57070e52b5a132f45f4be43adac6
EriRan <erik.rantanen@hotmail.fi>
2020-04-29 20:20:42 +0300

    Change route calculation response to a Map

--- src/components/route/sidebar/route/calculation/ResponseConverter.js
+++ src/components/route/sidebar/route/calculation/ResponseConverter.js
@@ -1,27 +1,33 @@
 /**
  * Convert the nodes response from calculator to a more compact format for state and element rendering.
+ *
+ * Return Map<String, NodeInfo>
+ *
+ * Key is made of the name of the start stop, a dash and the name of the destination stop. Eg. A-C
  */
 class ResponseConverter {
   convert(startStop, nodes) {
     if (nodes.length === 0) {
       return {
         totalDuration: null,
-        route: [],
+        route: null,
         message: "Reittiä ei löytynyt",
       };
     }
     return {
       totalDuration: nodes[nodes.length - 1].totalDuration,
       route: this.buildRoute(startStop, nodes),
+      message: null,
     };
   }
 
   buildRoute(startStop, nodes) {
-    const route = [];
+    const route = new Map();
     for (let i = 0; i < nodes.length; i++) {
       let currentNode = nodes[i];
       if (i === 0) {
-        route.push(
+        route.set(
+          this.createKey(startStop, currentNode.stopData.name),
           this.createOneDirection(
             startStop,
             currentNode.stopData.name,
@@ -31,7 +37,8 @@ class ResponseConverter {
         );
       } else {
         let previousNode = nodes[i - 1];
-        route.push(
+        route.set(
+          this.createKey(previousNode.stopData.name, currentNode.stopData.name),
           this.createOneDirection(
             previousNode.stopData.name,
             currentNode.stopData.name,
@@ -44,10 +51,14 @@ class ResponseConverter {
     return route;
   }
 
-  createOneDirection(from, to, line, duration) {
+  createKey(from, to) {
+    return from + "-" + to;
+  }
+
+  createOneDirection(to, from, line, duration) {
     return {
-      from: from,
       to: to,
+      from: from,
       line: line,
       duration: duration,
     };
