6d6a52f0bb169ae24e9090594a8b40f3908055cf
ornom0n <ornom0n@hotmail.com>
2020-04-29 21:06:51 +0300

    Add handling for route state

--- src/components/route/map/markers/road/RoadContainer.js
+++ src/components/route/map/markers/road/RoadContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import _ from "lodash";
 
 import { isUndefinedOrNull } from "../../../../../util/Utilities";
 
@@ -7,24 +8,26 @@ import RoadLine from "./RoadLine";
 
 class RoadContainer extends React.Component {
   render() {
-    const roads = Array.from(this.props.stops.values()).flatMap(
-      (stop) => stop.roads
-    );
     return (
       <g>
-        {roads
+        {Array.from(this.props.stops.values())
+          .flatMap((stop) => stop.roads)
           .filter((road) => {
             return road.isReverse === false;
           })
           .map((road) => {
-            return this.renderRoad(road, this.props.busStopLocationMap);
+            return this.renderRoad(
+              road,
+              this.props.busStopLocationMap,
+              this.props.calculatedRoute
+            );
           })}
         )}
       </g>
     );
   }
 
-  renderRoad(road, busStopLocationMap) {
+  renderRoad(road, busStopLocationMap, calculatedRoute) {
     const startPointLocation = busStopLocationMap.get(road.from.name);
     const endPointLocation = busStopLocationMap.get(road.to.name);
     if (isUndefinedOrNull(startPointLocation)) {
@@ -45,7 +48,20 @@ class RoadContainer extends React.Component {
       );
       return null;
     }
-
+    if (_.isMap(calculatedRoute) && calculatedRoute.length > 0) {
+      const calculatedRouteNode = calculatedRoute.get(road.from.name + "-" + road.to.name);
+      return (
+        <RoadLine
+          key={`road-line-from-${road.from.name}-to-${road.to.name}`}
+          roadData={road}
+          startPointLocation={startPointLocation}
+          endPointLocation={endPointLocation}
+          includesLines={road.includesLines}
+          calculatedRouteNode={calculatedRouteNode}
+          isRouteCalculated={true}
+        />
+      );
+    }
     return (
       <RoadLine
         key={`road-line-from-${road.from.name}-to-${road.to.name}`}
@@ -53,6 +69,7 @@ class RoadContainer extends React.Component {
         startPointLocation={startPointLocation}
         endPointLocation={endPointLocation}
         includesLines={road.includesLines}
+        isRouteCalculated={false}
       />
     );
   }
