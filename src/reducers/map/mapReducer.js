92b38344a52d60bf3f8d29466d3a70bfa08a922e
ornom0n <ornom0n@hotmail.com>
2020-05-01 14:17:16 +0300

    Add DO_SCROLL action handling

--- src/reducers/map/mapReducer.js
+++ src/reducers/map/mapReducer.js
@@ -1,13 +1,17 @@
+import { DO_SCROLL } from "../../actions/map/types";
+
 const INITIAL_STATE = {
   isScrolling: false,
   scrollLeft: null,
   scrollTop: null,
   clientX: null,
-  clientY: null
+  clientY: null,
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case DO_SCROLL:
+      return { ...state, scrollController: action.payload };
     default:
       return state;
   }
