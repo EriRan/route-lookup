9a6c1b7d53224806c44f083acf98b11bbe9bb8b4
ornom0n <ornom0n@hotmail.com>
2020-05-01 12:37:19 +0300

    Change to functional component

--- src/components/route/map/MapView.js
+++ src/components/route/map/MapView.js
@@ -3,15 +3,10 @@ import React from "react";
 import TrafficNetworkContainer from "./markers/TrafficNetworkContainer";
 import "./MapView.css";
 
-class MapView extends React.Component {
-  render() {
-    
-    return (
-      <svg className = "map-view" width="1500" height="1000">
-        <TrafficNetworkContainer stops={this.props.transportData.stops} />
-      </svg>
-    );
-  }
-}
-
-export default MapView;
+export default ({ transportData }) => {
+  return (
+    <svg className="map-view" width="1500" height="1000">
+      <TrafficNetworkContainer stops={transportData.stops} />
+    </svg>
+  );
+};
