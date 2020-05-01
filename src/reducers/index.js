b30d6a5c5031f43414b276ef801a6d1264401c99
ornom0n <ornom0n@hotmail.com>
2020-05-01 18:19:43 +0300

    Remove drag scrolling related state logic
    Could not get it to work

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import sidebarReducer from "./sidebar/sidebarReducer";
 import routeReducer from "./route/routeReducer";
-import mapReducer from "./map/mapReducer";
 
 export default combineReducers({
   form: formReducer,
-  map: mapReducer,
   sidebar: sidebarReducer,
   route: routeReducer,
 });
