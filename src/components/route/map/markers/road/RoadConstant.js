ba03478b4e5654923ab51e8c4c1535984f540d20
ornom0n <ornom0n@hotmail.com>
2020-05-01 13:16:17 +0300

    Different opacity for unused yellow line

--- src/components/route/map/markers/road/RoadConstant.js
+++ src/components/route/map/markers/road/RoadConstant.js
@@ -1,4 +1,7 @@
 export const LINE_GAP = 5;
 
 export const UNUSED_ROAD_OPACITY = 0.1;
+//Need a separate opacity for yellow because it gets almost invisible if
+//too much opacity is applied to it on gray background
+export const UNUSED_ROAD_OPACITY_YELLOW = 0.4;
 export const USED_ROAD_OPACITY = 1.0;
