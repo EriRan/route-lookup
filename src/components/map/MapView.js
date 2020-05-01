0ab88b84e5bab2f99e9590c6fb77f985ddda8c07
EriRan <erik.rantanen@hotmail.fi>
2020-05-01 19:55:09 +0300

    Make whole map visible even with sidebar on
    Not a pretty solution but works better than having
    the sidebar hide the first few stops

--- src/components/map/MapView.js
+++ src/components/map/MapView.js
@@ -6,10 +6,14 @@ import "./MapView.css";
 class MapView extends React.Component {
   render() {
     return (
-      <svg className = "map-view" width="2500" height="1000">
+      <svg className = "map-view" width="1800" height="1000">
         <TrafficNetworkContainer stops={this.props.transportData.stops} />
       </svg>
     );
+  }
+
+  componentDidMount() {
+    window.scrollTo(500, 0);
   }
 }
 
