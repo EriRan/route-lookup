4420bdc74d03fffcc10418d360d32a2921cdfb1f
ornom0n <ornom0n@hotmail.com>
2020-05-02 17:11:32 +0300

    Add TransportDataSingleton test

---
+++ src/data/TransportDataSingleton.test.js
@@ -0,0 +1,32 @@
+import TransportDataSingleton from "./TransportDataSingleton";
+
+test("Transport Data integration", () => {
+  const transportDataSingleton = TransportDataSingleton.getInstance();
+  expect(transportDataSingleton).toBeDefined();
+  expect(transportDataSingleton.stops).toBeInstanceOf(Map);
+  expect(transportDataSingleton.lines).toBeInstanceOf(Array);
+
+  validateStops(transportDataSingleton.stops);
+  validateHasLines(transportDataSingleton.lines);
+
+  function validateStops(stops) {
+    expect(stops.size).toBeGreaterThan(0);
+    Array.from(stops.values()).forEach((stop) => {
+      validateStopHasRoads(stop);
+    });
+  }
+
+  function validateStopHasRoads(stop) {
+    expect(stop.roads).toBeInstanceOf(Array);
+    expect(stop.roads.length).toBeGreaterThan(0);
+  }
+
+  function validateHasLines(lines) {
+    expect(lines).toBeInstanceOf(Array);
+    expect(lines.length).toBeGreaterThan(0);
+  }
+});
+
+test("Is singleton", () => {
+  expect(TransportDataSingleton.getInstance()).toEqual(TransportDataSingleton.getInstance());
+});
