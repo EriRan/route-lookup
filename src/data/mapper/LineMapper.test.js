d2977b5b70ae0fedc6b84083e5b995330eac071a
ornom0n <ornom0n@hotmail.com>
2020-05-02 15:09:51 +0300

    Add LineMapper test

---
+++ src/data/mapper/LineMapper.test.js
@@ -0,0 +1,20 @@
+import LineMapper from "./LineMapper";
+
+const mapper = new LineMapper();
+const mockLineData = createMockLinesJson();
+
+function createMockLinesJson() {
+  return {
+    lineOne: ["A", "B", "C"],
+    lineTwo: ["Z", "X", "Y"],
+    lineThree: ["C", "D", "Z"],
+  };
+}
+
+test("Can map lines", () => {
+  mapper.map(mockLineData).forEach((mappedLine) => {
+    expect(mappedLine.name).toBeDefined();
+    expect(mappedLine.stopsAt).toBeInstanceOf(Array);
+    expect(mappedLine.stopsAt.length).toBeGreaterThan(0);
+  });
+});
