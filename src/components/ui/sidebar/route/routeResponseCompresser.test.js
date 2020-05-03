708821b9835dc0f8cf39a56414b43f4a3b582757
ornom0n <ornom0n@hotmail.com>
2020-05-03 15:54:06 +0300

    Add test for route response compresser

---
+++ src/components/ui/sidebar/route/routeResponseCompresser.test.js
@@ -0,0 +1,55 @@
+import { compressResponse } from "./routeResponseCompresser";
+
+test("Empty array", () => {
+  const compressedResponse = compressResponse([
+  ]);
+  expect(compressedResponse).toBeInstanceOf(Array);
+  expect(compressedResponse).toHaveLength(0);
+});
+
+test("Single line path", () => {
+  const compressedResponse = compressResponse([
+    createOneRoute("A", "B", "line1"),
+    createOneRoute("B", "C", "line1"),
+  ]);
+  validateResponse(compressedResponse, 1);
+  validateSingleRoute(compressedResponse[0], "A", "C", "line1");
+});
+
+test("Multiple lines", () => {
+  const compressedResponse = compressResponse([
+    createOneRoute("A", "B", "line1"),
+    createOneRoute("B", "C", "line2"),
+    createOneRoute("C", "D", "line3"),
+    createOneRoute("D", "E", "line4"),
+  ]);
+  validateResponse(compressedResponse, 4);
+  validateSingleRoute(compressedResponse[0], "A", "B", "line1");
+  validateSingleRoute(compressedResponse[1], "B", "C", "line2");
+  validateSingleRoute(compressedResponse[2], "C", "D", "line3");
+  validateSingleRoute(compressedResponse[3], "D", "E", "line4");
+});
+
+function validateSingleRoute(
+  singleRoute,
+  expectedFrom,
+  expectedTo,
+  expectedLine
+) {
+  expect(singleRoute.from).toBe(expectedFrom);
+  expect(singleRoute.to).toBe(expectedTo);
+  expect(singleRoute.line).toBe(expectedLine);
+}
+
+function validateResponse(compressedResponse, expectedLength) {
+  expect(compressedResponse).toBeInstanceOf(Array);
+  expect(compressedResponse).toHaveLength(expectedLength);
+}
+
+function createOneRoute(fromName, toName, line) {
+  return {
+    from: fromName,
+    to: toName,
+    line: line,
+  };
+}
