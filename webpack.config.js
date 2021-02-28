0cc87095f4669cf18f0a3a05a1315731589f8ce9
Erik <erik.rantanen@hotmail.fi>
2021-02-28 19:10:30 +0200

    Add webpack cleanup plugin
    Clean the dist folder when a build is done

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,6 +9,12 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Kuutiola Reittiopas",
+    }),
+  ],
   module: {
     rules: [
       {
