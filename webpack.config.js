f5c5c594a36d1ac1c67952a663757100545ce49d
Erik <erik.rantanen@hotmail.fi>
2021-03-14 15:49:56 +0200

    Add favicon to the build

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "index.html",
+      favicon: "/public/favicon.ico",
     }),
   ],
   module: {
