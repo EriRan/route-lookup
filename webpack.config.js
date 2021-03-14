f8332053390a5cb50615c0008b1cb0a8fd18ab7d
Erik <erik.rantanen@hotmail.fi>
2021-03-14 18:51:28 +0200

    Fix live reload not working
    There is some kind of clash between webpack and
    browserslist definition done in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  target: "web",
   entry: "./src/index.js",
   devtool: "inline-source-map",
   devServer: {
