a413411acd29f8841832b794579f5726c1694331
Erik <erik.rantanen@hotmail.fi>
2021-03-22 11:27:23 +0200

    Add dev and production configs
    Create production build with npm run build
    and run dev build with npm start in a local dev server

--- webpack.common.js
+++ webpack.common.js
@@ -3,13 +3,8 @@ const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   target: "web",
   entry: "./src/index.js",
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist",
-  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
