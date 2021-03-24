93aae084de0120d054e90f2abd814e5f1f7677f2
Erik <erik.rantanen@hotmail.fi>
2021-03-24 18:38:23 +0200

    Convert TrafficNetworkContainer into Typescript
    Also some preliminary cleaning for the code. Could use
    some more cleaning though

--- src/components/map/markers/TrafficNetworkContainer.tsx
+++ src/components/map/markers/TrafficNetworkContainer.tsx
@@ -4,8 +4,10 @@ import _ from "lodash";
 import BusStop from "./stop/BusStop";
 import "./TrafficNetworkContainer.css";
 import RoadContainer from "./road/RoadContainer";
+import { TrafficMapProps } from "./types";
+import { BusStopLocation } from "../types";
 
-class BusTrafficContainer extends React.Component {
+class BusTrafficContainer extends React.Component<TrafficMapProps, {}> {
   render() {
     return (
       <g className="bus-traffic-container">{this.renderTrafficNetwork()}</g>
@@ -18,7 +20,7 @@ class BusTrafficContainer extends React.Component {
         <g className="bus-traffic-container">
           <RoadContainer
             busStopLocationMap={this.props.busStopLocationsMap}
-            stops={this.props.stops}
+            stops={this.props.stopMap}
           />
           {this.renderBusStops(this.props.busStopLocationsMap)}
         </g>
@@ -28,11 +30,12 @@ class BusTrafficContainer extends React.Component {
     }
   }
 
-  renderBusStops(busStopLocationsMap) {
+  private renderBusStops(busStopLocationsMap: Map<String, BusStopLocation>) {
     return (
       <g className="bus-stop-container">
         {Array.from(busStopLocationsMap.entries()).map((entry) => {
           return (
+            //entry[0] == name of the bus stop, entry[1] == coordinates of the bus stop
             <BusStop
               key={`stop-${entry[0]}`}
               name={entry[0]}
--- src/components/map/types.ts
+++ src/components/map/types.ts
@@ -1,5 +1,16 @@
 import { Stop } from "../../data/mapper/types";
 
-export type MapProps = {
+export interface MapProps {
   stopMap: Map<String, Stop>;
+}
+
+export type BusStopLocations = {
+  busStopLocationMap: Map<String, BusStopLocation>;
+  xMax: number;
+  yMax: number;
+};
+
+export type BusStopLocation = {
+  x: number;
+  y: number;
 };
