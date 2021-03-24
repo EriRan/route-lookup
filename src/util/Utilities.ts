b12df7ed0ce47c443891f4d4b6cbfd592b1f5856
Erik <erik.rantanen@hotmail.fi>
2021-03-24 08:25:01 +0200

    Convert Utilities into Typescript

--- src/util/Utilities.ts
+++ src/util/Utilities.ts
@@ -1,9 +1,9 @@
 import _ from "lodash";
 
-export function isUndefinedOrNull(value) {
+export function isUndefinedOrNull(value: any) {
   return _.isUndefined(value) || _.isNull(value);
 }
 
-export function isUndefinedOrNullOrEmptyString(value) {
+export function isUndefinedOrNullOrEmptyString(value: any) {
   return _.isUndefined(value) || _.isNull(value) || value === "";
 }
