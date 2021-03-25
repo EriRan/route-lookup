bea51d1239c84b408ae8f4acd769afad2fc64a54
Erik <erik.rantanen@hotmail.fi>
2021-03-25 14:37:00 +0200

    Convert RouteCards into Typescript
    Good exercise into converting functional components

--- src/components/ui/card/RouteCardContent.tsx
+++ src/components/ui/card/RouteCardContent.tsx
@@ -10,8 +10,9 @@ import {
 import RouteForm from "./route/RouteForm";
 import RouteResult from "./route/RouteResult";
 import RouteCardHeader from "./header/RouteCardHeader";
+import { UiContainerProps } from "../types";
 
-class RouteCardContent extends React.Component {
+class RouteCardContent extends React.Component<UiContainerProps, {}> {
   render() {
     return (
       <ExpansionPanel>
@@ -19,7 +20,7 @@ class RouteCardContent extends React.Component {
         <Divider />
         <ExpansionPanelDetails>
           <Grid direction="column">
-            <RouteForm possibleStops={this.props.transportData.stops} />
+            <RouteForm possibleStops={this.props.transportData.stopMap} />
             <Divider />
             <RouteResult transportData={this.props.transportData} />
           </Grid>
