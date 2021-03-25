a71f502312cbac6fb1182bc2a8a8d816f6ae45ec
Erik <erik.rantanen@hotmail.fi>
2021-03-25 14:41:07 +0200

    Convert RouteCardHeader into Typescript

--- src/components/ui/card/header/RouteCardHeader.tsx
+++ src/components/ui/card/header/RouteCardHeader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { ExpansionPanelSummary, Typography } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-class RouteCardHeader extends React.Component {
+class RouteCardHeader extends React.Component<{}, {}> {
   render() {
     return (
       <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
