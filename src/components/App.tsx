35a55b89d6b6d2afdb99cf27a673033f4d12f89f
Erik <erik.rantanen@hotmail.fi>
2021-03-27 13:20:55 +0200

    Add CssBaseline
    This provides a "good baseline for the css".
    It removed the gray background color but the rest of the app seems
    really nice. Let's try to bring the background color back later

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-
 import TransportDataSingleton from "../data/TransportDataSingleton";
 import UiContainer from "./ui/UiContainer";
 import MapView from "./map/MapView";
+import { CssBaseline } from "@material-ui/core";
 
 const App = () => {
   const transportData = TransportDataSingleton.getInstance();
   return (
-    <div className="ui container">
+    <div className="app">
+      <CssBaseline />
       <UiContainer transportData={transportData} />
       <MapView stopMap={transportData.stopMap} />
     </div>
