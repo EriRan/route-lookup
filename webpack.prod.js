1ec0a62e05af13ec27064da6d4b14846cf6e9da0
EriRan <erik.rantanen@hotmail.fi>
2021-07-04 17:00:43 +0300

    Remove source map from production
    No I don't think this is supposed to be here.

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,6 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
-  devtool: "source-map",
   optimization: {
     minimizer: [`...`, new CssMinimizerPlugin()],
   },
