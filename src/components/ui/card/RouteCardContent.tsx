99e8073776404c632ef954e3f2147df84e48bfd8
EriRan <erik.rantanen@hotmail.fi>
2022-01-25 18:58:39 +0200

    Turn class into hook

--- src/components/ui/card/RouteCardContent.tsx
+++ src/components/ui/card/RouteCardContent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { FunctionComponent } from "react";
 
 import { Divider, Accordion, Grid, AccordionDetails } from "@material-ui/core";
 
@@ -7,22 +7,22 @@ import RouteResult from "./route/RouteResult";
 import RouteCardHeader from "./header/RouteCardHeader";
 import { UiContainerProps } from "../types";
 
-class RouteCardContent extends React.Component<UiContainerProps, {}> {
-  render() {
-    return (
-      <Accordion>
-        <RouteCardHeader />
-        <Divider />
-        <AccordionDetails>
-          <Grid container direction="column">
-            <RouteForm stopMap={this.props.transportData.stopMap} />
-            <Divider />
-            <RouteResult />
-          </Grid>
-        </AccordionDetails>
-      </Accordion>
-    );
-  }
-}
+const RouteCardContent: FunctionComponent<UiContainerProps> = ({
+  transportData,
+}) => {
+  return (
+    <Accordion>
+      <RouteCardHeader />
+      <Divider />
+      <AccordionDetails>
+        <Grid container direction="column">
+          <RouteForm stopMap={transportData.stopMap} />
+          <Divider />
+          <RouteResult />
+        </Grid>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
 
 export default RouteCardContent;
