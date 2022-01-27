d75aa3495a94b96438dc6a8296f70ebad855e505
EriRan <erik.rantanen@hotmail.fi>
2022-01-27 18:33:55 +0200

    Add one test translation

--- src/components/ui/appbar/KuutiolaAppBarSubtitle.tsx
+++ src/components/ui/appbar/KuutiolaAppBarSubtitle.tsx
@@ -1,14 +1,21 @@
 import React, { FunctionComponent } from "react";
 
 import { Typography } from "@material-ui/core";
+import { useTranslation } from "react-i18next";
 
 /**
  * Secondary title that is displayed if using a development build
  */
-const KuutiolaAppBarSubtitle: FunctionComponent<{}> = () => (
-  <Typography variant="h6" align="center" color="secondary">
-    {process.env.NODE_ENV === "development" ? "Local development version" : ""}
-  </Typography>
-);
+const KuutiolaAppBarSubtitle: FunctionComponent<{}> = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Typography variant="h6" align="center" color="secondary">
+      {process.env.NODE_ENV === "development"
+        ? t('LOCAL_DEVELOPMENT_VERSION')
+        : ""}
+    </Typography>
+  );
+};
 
 export default KuutiolaAppBarSubtitle;
