c0b233df2c2445a5eba96ef29dfdff65263c06de
EriRan <erik.rantanen@hotmail.fi>
2022-01-27 18:33:47 +0200

    Change how translations are initialized

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,19 +2,12 @@ import TransportDataSingleton from "../data/TransportDataSingleton";
 import UiContainer from "./ui/UiContainer";
 import MapView from "./map/MapView";
 import { CssBaseline } from "@material-ui/core";
-import { initReactI18next, useTranslation } from "react-i18next";
-import i18n from "i18next";
-import translationsJson from "../../resources/translations/translations.json"
 
-i18n.use(initReactI18next)
-.init(translationsJson)
 
 const App = () => {
   const transportData = TransportDataSingleton.getInstance();
-  const { t } = useTranslation();
   return (
     <div className="app">
-      <h2>{t("Welcome to React")}</h2>
       <CssBaseline />
       <UiContainer transportData={transportData} />
       <MapView stopMap={transportData.stopMap} />
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import reduxThunk from "redux-thunk";
 import reducers from "./reducers";
+import './i18n';
 
 import App from "./components/App";
 
