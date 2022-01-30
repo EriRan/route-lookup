25676dd59b311eec667b2c2f8959d4b404a6318b
EriRan <erik.rantanen@hotmail.fi>
2022-01-30 16:21:29 +0200

    Remove temporary language change test

--- src/components/ui/appbar/KuutiolaAppTitle.tsx
+++ src/components/ui/appbar/KuutiolaAppTitle.tsx
@@ -1,8 +1,6 @@
 import { Typography, withStyles } from "@material-ui/core";
 import { FunctionComponent } from "react";
 import { useTranslation } from "react-i18next";
-import { useDispatch } from "react-redux";
-import { LANGUAGE_CHANGE } from "../../../actions/language/actions";
 import { KuutiolaAppBarProps } from "./types";
 
 const styles = {
@@ -15,16 +13,10 @@ const KuutiolaAppTitle: FunctionComponent<KuutiolaAppBarProps> = ({
   classes,
 }) => {
   const { t } = useTranslation();
-  const dispatch = useDispatch();
   return (
-        <Typography
-          variant="h5"
-          align="center"
-          className={classes.centeredTitle}
-          onClick={() => dispatch({type: LANGUAGE_CHANGE, payload: {language: "en"}})}
-        >
-          {t('APP_TITLE')}
-        </Typography>
+    <Typography variant="h5" align="center" className={classes.centeredTitle}>
+      {t("APP_TITLE")}
+    </Typography>
   );
 };
 
