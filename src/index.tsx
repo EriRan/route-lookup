fb2afa33cb1ddbc814fa9d844ebde97bae1b1888
EriRan <erik.rantanen@hotmail.fi>
2022-03-28 19:11:22 +0300

    Create store in one ts file
    I want to be able to use this in my component tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,14 @@
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import reduxThunk from "redux-thunk";
-import reducers from "./reducers";
 import './i18n';
 
 import App from "./components/App";
+import createRouteLookupStore from "./reducers/createStore";
 
-const composeEnhancers =
-  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  reducers,
-  composeEnhancers(applyMiddleware(reduxThunk))
-);
+
 
 render(
-  <Provider store={store}>
+  <Provider store={createRouteLookupStore()}>
     <App />
   </Provider>,
   document.querySelector("#root")
