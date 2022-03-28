d9bde1bc1c755aa9eb7d52c2aa7622e63f4498df
EriRan <erik.rantanen@hotmail.fi>
2022-03-28 19:17:37 +0300

    Add comments

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -7,9 +7,11 @@ import reduxThunk from "redux-thunk";
 export default function createRouteLookupStore() {
   const composeEnhancers =
     (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  // Creation of root reducers
   const reducers = combineReducers({
     route: ROUTE_REDUCERS,
     language: LANGUAGE_REDUCERS,
   });
+  // Apply reducers and middleware to the store
   return createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 }
