565f268cc21905fd147c3a9b08d0e00be176da1a
EriRan <erik.rantanen@hotmail.fi>
2022-03-28 19:16:00 +0300

    Do store creation in one file
    Pretty verbose but its just 15 lines

--- src/reducers/createStore.ts
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, compose, createStore } from "redux";
-import reducers from ".";
-import reduxThunk from "redux-thunk";
-
-export default function createRouteLookupStore() {
-  const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  return createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
-};
